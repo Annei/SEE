3db3459c38c370e1d1c8c2cc3a10590aa1d387a0
Gustavo Jimenez <GustavoAngelMartinezJimenez@outlook.es>
2019-07-05 23:46:54 -0500

    Actualizacion de vistas
    Integracion de cambios en los archivos en public y modificacion de las vistas login y datosgenerales.

--- Framework/views/alumnos/datosGenerales.php
+++ Framework/views/alumnos/datosGenerales.php
@@ -10,72 +10,113 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="<?php echo constant('URL'); ?>public/css/style.css"></link>
+    <link rel="stylesheet" type="text/css" href="<?php echo constant('URL'); ?>public/css/style.css">
     <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/fontawesome/css/all.css">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> -->
     <title>SEE - Perfil</title>
     <link rel="shortcut icon" href="">
 </head>
 <body>
-    <div class="main flex">
-        <div class="column profile">
+    <div class="main flex ">
+        <div class="column profile full">
             <div class="row-responsive">
-            <div class="column menu-left align-center justify-center">
+                <div class="column align-center justify-center menu" tabindex="0">
+                        <div class="smartphone-menu-trigger"></div>
                         <div class="white-space-24"></div>
-                        <div class="responsive-img item-left justify-center align-center">
-                            <img src="<?php echo constant('URL'); ?>public/img/upqroo-newlogo@2x.png" alt="responsive img" title="responsive img" class="cover-img "/>
+    
+                        <div class="responsive-img item-left justify-center align-center logo">
+                            <img src="<?php echo constant('URL'); ?>/public/img/upqroo-newlogo@2x.png" alt="responsive img" title="responsive img" class="cover-img "/>
                         </div>
+    
                         <div class="white-space-32"></div>
-                        <div class="white-space-16"></div>
+                        <!-- <div class="white-space-16"></div> -->
+                        
                         <div class="column main align-center auto">
-                            <div class="row item-left justify-center align-start">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>alumnos/datos">Datos generales</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>alumnos/carga-academica">Carga académica</a></h4>
-                                </div>                        
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>alumnos/calificaciones">Calificaciones</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class="column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>alumnos/kardex">Kardex</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>horario/horario">Horario</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>xd">Noticias</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>xd">Formatos</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>xd">Correo Institucional</a></h4>
-                                </div>
-                            </div>
-                            <div class="row item-left justify-center">
-                                <div class = "column">
-                                    <h4 class="color-white weight-regular font-small"><a href="<?php echo constant('URL'); ?>logout">Cerrar Sesión</a></h4>
-                                </div>
+                            <div class="menu-options">
+                                <a href="<?php echo constant('URL'); ?>alumnos/datos" class="item-left">
+                                    <div class="row justify-center align-start">
+                                        <div class="column icon align-center"><i class="fa fa-user"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Datos generales</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                                
+                                <a href="<?php echo constant('URL'); ?>alumnos/carga-academica" class="item-left">
+                                    <div class="row justify-center">
+                                            <div class="column icon align-center"><i class="fa fa-university"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Carga académica</h4>
+                                        </div>                        
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>alumnos/calificaciones" class="item-left">
+                                    <div class="row justify-center">
+                                            <div class="column icon align-center"><i class="fa fa-star"></i></div> 
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Calificaciones</h4>
+                                        </div>
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>alumnos/kardex" class="item-left">
+                                    <div class="row justify-center">
+                                        <div class="column icon align-center"><i class="fa fa-book-user"></i></div>
+                                        <div class="column full">
+                                            <h4 class="color-white weight-regular font-small">Kardex</h4>
+                                        </div>
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>horario/horario" class="item-left">
+                                    <div class="row justify-center">
+                                        <div class="column icon align-center"><i class="fa fa-calendar-alt"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Horario</h4>
+                                        </div>
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>xd" class="item-left">
+                                    <div class="row justify-center">
+                                        <div class="column icon align-center"><i class="fa fa-newspaper"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Noticias</h4>
+                                        </div>
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>alumnos/formatos" class="item-left">
+                                    <div class="row justify-center">
+                                        <div class="column icon align-center"><i class="fa fa-file-download"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Formatos</h4>
+                                        </div>
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>xd" class="item-left">
+                                    <div class="row justify-center">
+                                        <div class="column icon align-center"><i class="fa fa-mail-bulk"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Correo Institucional</h4>
+                                        </div>
+                                    </div>
+                                </a>
+    
+                                <a href="<?php echo constant('URL'); ?>logout" class="item-left">
+                                    <div class="row item-left justify-center">
+                                        <div class="column icon align-center"><i class="fa fa-sign-out"></i></div>
+                                        <div class = "column full">
+                                            <h4 class="color-white weight-regular font-small">Cerrar Sesión</h4>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-                </div>
-                <div class="column">
+                </div>      
+                <div class="column full container-gnral-info body">
                     <div class="row-responsive">
                             <div class="row-responsive header-general-data align-center" style="background-image: url(<?php echo constant('URL'); ?>public/img/new-footer-blue.png)">
                                 <div class="column space-header">
@@ -93,7 +134,7 @@
                                 </div>
                         </div>
                     </div>
-                    <div class="row-responsive">
+                    <div class="row-responsive container-gnral-info">
                         <div class="column container-info-student justify-start">
                             <div class="white-space-24"></div>
                             <div class="row-responsive justify-center">
@@ -119,7 +160,7 @@
                                         Regular
                                     </p>
                                     <div class="white-space-24"></div>
-                                    <h4 class="color-darkgray weight-bold font-small">Situaci&oacute;n de vigencia</h4>
+                                    <h4 class="color-darkgray weight-bold font-small">Año de ingreso</h4>
                                     <p class="color-darkgray weight-regular">
                                         SEP-DIC2016
                                     </p>
@@ -137,7 +178,7 @@
                             <h2 class="color-darkBlue weight-bold">Datos generales del estudiante</h2>
                                 <form class="column frm-info-gral">
                                     <div class="row-responsive break-ipad">
-                                        <div class="column ">
+                                        <div class="column align-start">
                                             <div class="column container-input-info-gral">
                                             <h4 class="color-darkgray font-regular">Fecha de Nacimiento</h4>
                                             <input type="text" name="txtFechaNac" placeholder="<?php echo $this->datos['cumple']; ?>" class="input input-info-profile" required>
@@ -165,7 +206,7 @@
                                     </div>
                                     <div class="white-space-16"></div>
                                     <div class="row-responsive break-ipad">
-                                        <div class="column">
+                                        <div class="column align-start">
                                             <div class="column container-input-info-gral" >
                                             <h4 class="color-darkgray font-regular">Telefono Domicilio</h4>
                                             <input type="text" name="txtTelDom" placeholder="018006971" class="input input-info-profile" required>
@@ -186,9 +227,7 @@
                                     </div>
                                     <div class="white-space-32"></div>
                                     <div class="white-space-8"></div>
-                                    <div class="row">
-                                        <button type="submit" class="btn btn-general-data  bg-darkBlue color-white font-regular weight-semi">Modificar datos</button>
-                                    </div>
+                                   
                                     <div class="white-space-64"></div>
                                 </form>
                             </div>
