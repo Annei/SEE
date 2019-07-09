17cce2f5e1b575b4e68e64e374d8667ec9a9f630
olopez <olopez@sunset.com.mx>
2019-07-09 11:02:07 -0500

    ajustes alert

--- Framework/views/alerts/Headers.php
+++ Framework/views/alerts/Headers.php
@@ -4,4 +4,3 @@
       <script type="text/javascript" src="<?php echo constant('URL'); ?>/public/jquery/js/sweetalert2.min.js"></script>    
       <link rel="stylesheet" href="<?php echo constant('URL'); ?>/public/jquery/css/sweetalert2.css" type="text/css">
       <link rel="stylesheet" href="<?php echo constant('URL'); ?>/public/jquery/css/sweetalert2.min.css" type="text/css">
-      <link rel="stylesheet" href="<?php echo constant('URL'); ?>/public/jquery/css/bootstrap.min.css" type="text/css">
